d4c42e3ead57b46209a75d495c4de896eba25501
slaymed <mohabedr34@gmail.com>
2024-08-17 14:14:28 +0100

    Hello world

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <iframe
-      src="https://slotslaunch.com/iframe/30460?token=giuglU5K9kkKUODmVBW4roeg9G5IFBHhHvxRQnevuZ81YfxK2Z"
+      src="https://slotslaunch.com/iframe/5483?token=giuglU5K9kkKUODmVBW4roeg9G5IFBHhHvxRQnevuZ81YfxK2Z"
       width={720}
       height={480}
     />
