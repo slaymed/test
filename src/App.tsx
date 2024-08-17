0dfc15e047f77f36354f27761a817b1228dacc3e
slaymed <mohabedr34@gmail.com>
2024-08-17 14:17:36 +0100

    Hello world

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <iframe
-      src="https://slotslaunch.com/iframe/5483?token=giuglU5K9kkKUODmVBW4roeg9G5IFBHhHvxRQnevuZ81YfxK2Z"
-      width={720}
-      height={480}
-    />
+    <>
+      <iframe
+        src="https://slotslaunch.com/iframe/5483?token=giuglU5K9kkKUODmVBW4roeg9G5IFBHhHvxRQnevuZ81YfxK2Z"
+        width={720}
+        height={480}
+      />
+      <iframe src="https://mohamedbedr.com" width={720} height={480} />
+    </>
   );
 }
 
