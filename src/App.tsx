2eee3793ddcf7c97b1b190ee8fe336b14e92c46c
slaymed <mohabedr34@gmail.com>
2024-08-17 14:10:49 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <iframe
+      src="https://slotslaunch.com/iframe/30460?token=giuglU5K9kkKUODmVBW4roeg9G5IFBHhHvxRQnevuZ81YfxK2Z"
+      width={720}
+      height={480}
+    />
+  );
+}
+
+export default App;
